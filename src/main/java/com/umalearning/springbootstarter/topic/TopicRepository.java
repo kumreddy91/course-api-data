a3c40ab9c4b07ec70b2b29c363bfd7635d8044dd
uma maheswar <uma maheswar>
2017-05-03 16:45:53 -0600

    Initial Commit

---
+++ src/main/java/com/umalearning/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.umalearning.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
